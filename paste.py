35dca709b5d26d6dc81bfef4796c5e833c27d75a
Luiz Carlos Batista de Cerqueira <lcerqueira@uolinc.com>
2020-11-24 12:43:06 -0300

    Passando o delay como parâmetro

--- paste.py
+++ paste.py
@@ -1,16 +1,24 @@
-from input import KeyboardInput
+import argparse
 import subprocess
 
+from input import KeyboardInput
+
+
 def getClipboardData():
-    p = subprocess.Popen(['xclip','-selection', 'clipboard', '-o'], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['xclip', '-selection', 'clipboard', '-o'], stdout=subprocess.PIPE)
     retcode = p.wait()
     data = p.stdout.read()
     return data
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--delay', metavar='N', type=int, help='Time delay to paste')
+args = parser.parse_args()
+
+delay = args.delay or float(2)
+
 text = str(getClipboardData())
 text = text[2:-1]
-
-delay = float(2)
 
 keyboard = KeyboardInput()
 keyboard.type_string(text, delay)
