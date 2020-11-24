0121c934be16e566fe62a6d6a00eeb4b531ff6d6
Luiz Carlos Batista de Cerqueira <lcerqueira@uolinc.com>
2020-11-24 12:46:21 -0300

    Adicionando opção de retornar texto em caixa alta (uppercase)

--- paste.py
+++ paste.py
@@ -4,21 +4,23 @@ import subprocess
 from input import KeyboardInput
 
 
-def getClipboardData():
+def get_clipboard_data():
     p = subprocess.Popen(['xclip', '-selection', 'clipboard', '-o'], stdout=subprocess.PIPE)
     retcode = p.wait()
     data = p.stdout.read()
-    return data
+    return str(data)[2:-1]
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--delay', metavar='N', type=int, help='Time delay to paste')
+parser.add_argument('-u', '--uppercase', action='store_true', help='Convert text to Uppercase')
 args = parser.parse_args()
 
 delay = args.delay or float(2)
 
-text = str(getClipboardData())
-text = text[2:-1]
+text = get_clipboard_data()
+if args.uppercase:
+    text = text.upper()
 
 keyboard = KeyboardInput()
 keyboard.type_string(text, delay)
